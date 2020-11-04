ee75638d2ad7d4ba2438cda939d7912ff1016ab4
juseong_kim <dcjuja@gmail.com>
2020-11-04 12:25:26 +0900

    feat: quasar add

---
+++ src/quasar.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+
+import "./styles/quasar.scss";
+import "quasar/dist/quasar.ie.polyfills";
+import lang from "quasar/lang/en-US.js";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import { Quasar } from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {},
+  lang: lang
+});
